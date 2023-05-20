8bde9b59d4d17140d540d3d6956d71b339036b1b
jeremy <j.e.dicker1990@gmail.com>
2023-05-20 12:08:30 +0100

    Implemented Acceptance Tests for project and task controllers

--- src/test/java/com/lazy/todo/SpringBootTodoProjectAcceptanceTests.java
+++ src/test/java/com/lazy/todo/SpringBootTodoProjectAcceptanceTests.java
@@ -1,13 +1,7 @@
 package com.lazy.todo;
 
 import com.google.gson.Gson;
-import com.lazy.todo.controllers.TaskController;
-import com.lazy.todo.models.Task;
-import com.lazy.todo.payload.request.SignupRequest;
-import com.lazy.todo.payload.request.TaskRequest;
-import com.lazy.todo.repository.TaskRepository;
-import com.lazy.todo.security.jwt.JwtUtils;
-import com.lazy.todo.services.TaskService;
+import com.lazy.todo.payload.request.ProjectRequest;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
@@ -15,27 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import javax.transaction.TransactionScoped;
 import javax.transaction.Transactional;
-
 import java.util.Date;
-import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.oneOf;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
-public class SpringBootTodoTaskAcceptanceTests {
+public class SpringBootTodoProjectAcceptanceTests {
 
     @Autowired
     MockMvc mockMvc;
@@ -43,19 +32,15 @@ public class SpringBootTodoTaskAcceptanceTests {
     @Autowired
     Gson gson;
 
+    ProjectRequest PROJECT_REQUEST_1 = new ProjectRequest("testTitle", "testDescription");
+    ProjectRequest PROJECT_REQUEST_2 = new ProjectRequest("testTitle2", "testDescription2");
+
+
     @Value("${lazy.app.jwtSecret}")
     private String jwtSecret;
 
     @Value("${lazy.app.jwtExpirationMs}")
     private int jwtExpirationMs;
-
-    TaskRequest taskRequest = new TaskRequest("title", "description");
-    TaskRequest TASK_REQUEST_2 = new TaskRequest("title2", "description2");
-
-    Task TASK_1 = new Task("title1", "description1");
-    Task TASK_2 = new Task("title2", "description2");
-    Task TASK_3 = new Task("title3", "description3");
-    Task TASK_4 = new Task("title4", "description4");
 
     public String generateJwtToken() {
 
@@ -72,109 +57,103 @@ public class SpringBootTodoTaskAcceptanceTests {
     @WithMockUser(username = "testUser2")
     public void newTaskTest() throws Exception {
         mockMvc.perform(
-                        post("/api/task/new")
+                        post("/api/project/new")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .header("Authorization", "bearer:" + generateJwtToken())
-                                .content(gson.toJson(taskRequest))
+                                .content(gson.toJson(PROJECT_REQUEST_1))
                                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title", is(taskRequest.getTitle())))
+                .andExpect(jsonPath("$.title", is(PROJECT_REQUEST_1.getTitle())))
                 .andReturn().getResponse().getContentAsString();
     }
 
-    /**
-     * KLUDGE!!! have manually entered a task here which fits this test requirement LIABLE TO BREAK
-     * TODO setup/teardown of database programmatically
-     *
-     * @throws Exception
-     */
     @Test
     @WithMockUser(username = "testUser2")
-    public void getTaskByIdTest() throws Exception {
-        String jwt = "bearer:" + generateJwtToken();
+    public void getProjectByIdTest() throws Exception {
         mockMvc.perform(
-                        get("/api/task/1")
+                        get("/api/project/2")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .header("Authorization", jwt)
-                                .content(gson.toJson(taskRequest))
+                                .header("Authorization", "bearer:" + generateJwtToken())
                                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title", is(TASK_1.getTitle())))
+                .andExpect(jsonPath("$.title", is(PROJECT_REQUEST_1.getTitle())))
                 .andReturn().getResponse().getContentAsString();
     }
 
     @Test
     @WithMockUser(username = "testUser2")
-    public void getAllTasksTest() throws Exception {
-        String jwt = "bearer:" + generateJwtToken();
+    public void getAllProjectsTest() throws Exception {
         mockMvc.perform(
-                        get("/api/task/all")
+                        get("/api/project/all")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .header("Authorization", jwt)
-                                .content(gson.toJson(taskRequest))
+                                .header("Authorization", "bearer:" + generateJwtToken())
                                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].title", oneOf("title", "title3",
-                        "title4", "title2")))
-                .andExpect(jsonPath("$.[1].title", oneOf("title", "title3",
-                        "title4", "title2")))
-                .andExpect(jsonPath("$.[2].title", oneOf("title", "title3",
-                        "title4", "title2")))
-                .andReturn().getResponse().getContentAsString();
-    }
-
-
-    @Test
-    @WithMockUser(username = "testUser2")
-    public void getTaskUsersTest() throws Exception {
-        String jwt = "bearer:" + generateJwtToken();
-        mockMvc.perform(
-                        get("/api/task/users/1")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .header("Authorization", jwt)
-                                .content(gson.toJson(taskRequest))
-                                .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0]", is("testUser2")))
+                .andExpect(jsonPath("$.[0].title", is(PROJECT_REQUEST_1.getTitle())))
                 .andReturn().getResponse().getContentAsString();
     }
 
     @Test
-    @WithMockUser(username = "testUser2")
     @Transactional
-    public void getUpdateTaskTest() throws Exception {
-        String jwt = "bearer:" + generateJwtToken();
-        mockMvc.perform(
-                        put("/api/task/1")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .header("Authorization", jwt)
-                                .content(gson.toJson(TASK_REQUEST_2))
-                                .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title", is(TASK_REQUEST_2.getTitle())))
-                .andReturn().getResponse().getContentAsString();
-    }
-
-    @Test
     @WithMockUser(username = "testUser2")
-    @Transactional
-    public void deleteTaskTest() throws Exception {
-        String jwt = "bearer:" + generateJwtToken();
+    public void updateProjectByIdTest() throws Exception {
+        //check that the project is the expected initial values
         mockMvc.perform(
-                        delete("/api/task/1")
+                        get("/api/project/2")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .header("Authorization", jwt)
-                                .content(gson.toJson(TASK_REQUEST_2))
+                                .header("Authorization", "bearer:" + generateJwtToken())
                                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title", is(taskRequest.getTitle())))
+                .andExpect(jsonPath("$.title", is(PROJECT_REQUEST_1.getTitle())))
+                .andReturn().getResponse().getContentAsString();
+        //change the values
+        mockMvc.perform(
+                        put("/api/project/2")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "bearer:" + generateJwtToken())
+                                .content(gson.toJson(PROJECT_REQUEST_2))
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is(PROJECT_REQUEST_2.getTitle())))
+                .andReturn().getResponse().getContentAsString();
+        //check the values have changed properly
+        mockMvc.perform(
+                        get("/api/project/2")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "bearer:" + generateJwtToken())
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is(PROJECT_REQUEST_2.getTitle())))
                 .andReturn().getResponse().getContentAsString();
     }
-
+    @Test
+    @Transactional
+    @WithMockUser(username = "testUser2")
+    public void deleteProjectByIdTest() throws Exception {
+        //start with a delete request and check that the expected task is returned
+        mockMvc.perform(
+                        delete("/api/project/2")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "bearer:" + generateJwtToken())
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is(PROJECT_REQUEST_1.getTitle())))
+                .andReturn().getResponse().getContentAsString();
+        //request the deleted task to verify it no longer exists
+        mockMvc.perform(
+                        get("/api/project/2")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "bearer:" + generateJwtToken())
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent())
+                .andReturn().getResponse().getContentAsString();
+    }
 }
